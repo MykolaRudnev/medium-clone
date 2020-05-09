6aa8e52a95d0e8f48f8a9e3cf65ad565bdd84525
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-09 16:34:57 +0200

    added top bar and connect styles

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-import Routes from 'routes';
+import Routes from 'routes'
+import TopBar from 'components/topBar'
 
-
-const  App = ()  => {
-  return(
-    <div>
-      <h3>Welcome to hooks</h3>
+const App = () => {
+  return (
+    <div className="App">
+     
       <Router>
+        <TopBar />
         <Routes />
       </Router>
     </div>
   )
 }
 
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
