e5155ff83c523149b092150cd41ae663fa069f83
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-13 21:07:59 +0200

    getting the current user

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from 'routes'
 import TopBar from 'components/topBar'
 import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserCheker from 'components/currentUserChecker'
 
 const App = () => {
   return (
     <CurrentUserProvider>
+      <CurrentUserCheker>
       <Router>
         <TopBar />
         <Routes />
       </Router>
+      </CurrentUserCheker>
     </CurrentUserProvider>
   )
 }
